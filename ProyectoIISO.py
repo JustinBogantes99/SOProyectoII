d9cfde033d53d74ce2224bc445e9593788d623d6
Justin Bogantes <justin.bogantes99@gmail.com>
2022-11-01 23:27:06 -0600

    IU

--- ProyectoIISO.py
+++ ProyectoIISO.py
@@ -2,14 +2,16 @@ from os import path as path2
 from cgitb import text
 from glob import glob
 from importlib.resources import path
-from multiprocessing.connection import wait
 from time import sleep
 from tkinter import *
 from tkinter import ttk
 from unittest.mock import patch #Libreria para crear la interfaz grafica
 from tkinter import messagebox
 from tkinter import filedialog as fd
+import tkinter as tk
+
 import random
+from PIL import Image,ImageTk
 
 fileContent=[]
 
@@ -17,36 +19,28 @@ def validate_entry(text):
     return text.isdecimal()
 
 def select_file():
+    global FileNameLabel
     filetypes = (
         ('text files', '*.txt'),
         ('All files', '*.*')
     )
     lines=[]
-    with fd.askopenfile(filetypes=filetypes) as f:
-        lines = [line.rstrip('\n') for line in f]
-    
+    try:
+        with fd.askopenfile(filetypes=filetypes) as f:
+            FileNameLabel.config(text=f.name)
+            lines = [line.rstrip('\n') for line in f]
+    except:
+        print("Error")
     listaProcesos=[]
     for x in range(len(lines)):
         listaProcesos+=[lines[x].split(", ")]
-    print(listaProcesos)
+    fileContent=listaProcesos
+    print(fileContent)
 
 def ventana_iniciar(ventana):
-    global txt_charge, mensajeCargado
-    frme_venta_iniciar=Frame(ventana,width=750,height=695,bg="#FFE4B5")
-    frme_venta_iniciar.place(x=0,y=0)
-    mensajeCargado=Label(ventana,text="",bd=10,bg="#FFE4B5")
-    mensajeCargado.place(x=470,y=635)
+    pass
 
-    open_button = ttk.Button(
-        ventana,
-        text='Open Files',
-        command=select_file
-    )
-    open_button.place(x=470,y=635)
-    seed_text=ttk.Entry(width = 20,validate="key",validatecommand=(ventana.register(validate_entry), "%S"))
-    seed_text.place(x=340,y=645)
-    btn_cargar=Button(ventana,text="Iniciar Simulacion",command=lambda:[ ],highlightthickness=1,bd=3,activebackground="khaki1")
-    btn_cargar.place(x=220,y=640)
+    
 
 def cargar(ventana):
     global txt_charge, mensajeCargado
@@ -57,11 +51,29 @@ def cargar(ventana):
     else:
         mensajeCargado['text'] = "No se ha encontrado el .txt"
     ventana.update()
-        
 ventana_principal=Tk()  
 ventana_principal.title("Memory")     
 ventana_principal.geometry("1400x720+280+0")
-ventana_principal.config(bg="#FFE4B5")
 ventana_principal.resizable(0,0)
-ventana_iniciar(ventana_principal)
+img=ImageTk.PhotoImage(file="name.png")
+imgbtn=ImageTk.PhotoImage(file="simu.png")  
+LocalImg=Label(ventana_principal,image=img)
+LocalImg.place(x=100,y=100)
+open_button = ttk.Button(
+        ventana_principal,
+        text='Open Files',
+        command=select_file
+    )
+open_button.place(x=350,y=380)
+seedLabel=Label(ventana_principal,text="Por favor ingrese la semilla")
+seedLabel.place(x=100,y=300)
+seed_text=ttk.Entry(width = 20,validate="key",validatecommand=(ventana_principal.register(validate_entry), "%S"))
+seed_text.place(x=260,y=300)
+FileLabel=Label(ventana_principal,text="Seleccion el archivo a cargar")
+FileLabel.place(x=100,y=380)
+FileNameLabel=Label(ventana_principal,text="")
+FileNameLabel.place(x=100,y=400)
+btn_cargar=Button(ventana_principal,image=imgbtn,command=lambda:[ventana_iniciar(ventana_principal)],highlightthickness=1,bd=3)
+btn_cargar.place(x=600,y=640)
+
 ventana_principal.mainloop() 

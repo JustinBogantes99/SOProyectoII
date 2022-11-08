2e34d6144d8bc486d850c5cdd35d5c2122b10e16
jblancor12 <jablancr@gmail.com>
2022-11-07 21:26:57 -0600

    mas avances en optimo

--- Optimo.py
+++ Optimo.py
@@ -27,17 +27,39 @@ listaAccesosBarajados=[[1,1,500]
 ,[3,8,512]
 ,[3,9,512]
 ,[3,7,1024]
+,[3,6,128]
+,[1,2,1024]
+,[2,4,256]
+,[2,5,512]
+,[4,10,256]
+,[1,3,512]
+,[3,7,1024]
+,[4,10,256]
+,[3,8,512]
+,[1,1,500]
+,[2,5,512]
+,[3,8,512]
+,[3,9,512]
+,[3,7,1024]
 ]
 
-
-pRAM=[]
-pVRAM=[]
-paginasMarcadas=[]
+RAMSize=5
+RAMTotal=RAMSize*4
+RAMUtilizada=0
+VRAMUtilizada=len
+FragmentacionInternaOpt=0
+TiempoSimOpt=0
+TiempoTrashingOpt=0
+RAMOpt=[]
+VRAMOpt=[]
+VRAMUtilizada=len(VRAMOpt)*4
+paginasMarcadas=[]  #lista con las paginas y la cantidad de accesos a memoria necesarios antes de ser usada [accesos,[pagina]]
 for acceso in ListadeAccesos:
     paginasMarcadas.append([0,acceso])
 
 
 
+#Calcula cuantos accesos a memoria faltan para que la paginas sea usada de nuevo
 def calcularAccesosfaltanttes():
         for x in range(len(ListadeAccesos)):
             contador=0
@@ -48,33 +70,82 @@ def calcularAccesosfaltanttes():
                     break 
             paginasMarcadas[x][0]=contador
 
-        
+#Devuelve la cantidad de accesoss faltantes para utilizar la pagina ptr       
 def getFaltantes(ptr):
     for y in range( len(paginasMarcadas)):
         if paginasMarcadas[y][1][1]==ptr:
             return paginasMarcadas[y][0]
 
+
+
+def calcularFragmentacionInternaOpt():
+    global FragmentacionInternaOpt
+    FragmentacionInternaOpt=0
+    for pagina in RAMOpt:
+        FragmentacionInternaOpt= FragmentacionInternaOpt + ((4000-pagina[2])/1000)
+    return FragmentacionInternaOpt
+
+def calcularRAMUtilizadaYVRAM():
+    global RAMUtilizada, VRAMUtilizada
+    RAMUtilizada=len(RAMOpt*4)
+    VRAMUtilizada = len(VRAMOpt*4)
+
 def optimo():
+    global TiempoSimOpt, TiempoTrashingOpt
     while(len(listaAccesosBarajados)>1):
-        if len(pRAM)<10:
-            pRAM.append(listaAccesosBarajados.pop(0))
+        if len(RAMOpt)<RAMSize:
+            siguiente=listaAccesosBarajados.pop(0)
+           
+            if RAMOpt.count(siguiente)==0:
+                if VRAMOpt.count(siguiente)>0:
+                    VRAMOpt.remove(siguiente)
+                    RAMOpt.append(siguiente)
+                    TiempoSimOpt=TiempoSimOpt+5
+                    TiempoTrashingOpt=TiempoTrashingOpt+5
+
+                RAMOpt.append(siguiente)
+                TiempoSimOpt=TiempoSimOpt+1
+            
+            else:
+                TiempoSimOpt=TiempoSimOpt+1
+
+
+
             sleep(2)
 
-        if len(pRAM)==10:
-            max=0 #index de la pagina mas tardada
-            for x in range(len(pRAM)):
-                accessosfaltantes =getFaltantes(pRAM[x][1])
-                if accessosfaltantes>max:
-                    max= x
+        if len(RAMOpt)>=RAMSize:
+            siguiente= listaAccesosBarajados.pop(0)
+            if RAMOpt.count(siguiente)==0:
+                maxIndx=0 #index de la pagina mas tardada
+                max=0 #accesos faltantes
+                for x in range(len(RAMOpt)):
+                    accessosfaltantes =getFaltantes(RAMOpt[x][1])
+                    if accessosfaltantes>max:
+                        max= accessosfaltantes
+                        maxIndx=x
 
-            pVRAM.append(pRAM[x])
-            pRAM[x]=listaAccesosBarajados.pop(0)
+                siguiente= listaAccesosBarajados.pop(0)
+                if VRAMOpt.count(siguiente)>0:
+                    VRAMOpt.remove(siguiente)
+                VRAMOpt.append(RAMOpt[maxIndx])
+                RAMOpt[maxIndx]=siguiente
+                TiempoTrashingOpt=TiempoTrashingOpt+5
+                TiempoSimOpt=TiempoSimOpt+6
+            
+            TiempoSimOpt=TiempoSimOpt+1
+
             sleep(2)
 
         calcularAccesosfaltanttes()
+        calcularFragmentacionInternaOpt()
+        calcularRAMUtilizadaYVRAM()
+        print("Tiempo total: ",TiempoSimOpt)
+        print("Tiempo de Trashing: ",TiempoTrashingOpt)
+        print("RAM utilizada: ", RAMUtilizada)
+        print("VRAM utilizada: ", VRAMUtilizada)
 
-        print(pRAM)
-        print(pVRAM)
+        #print("RAM-",RAMOpt)
+        #print("VRRAM-",VRAMOpt)
 
 
 

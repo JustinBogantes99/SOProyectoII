62e7ac60975b81f79c3e953cf6e1cd7007802486
JustinBogantes99 <justin.bogantes99@gmail.com>
2022-11-11 20:36:11 -0600

    LRU cambios

--- Algoritmos/LRU.py
+++ Algoritmos/LRU.py
@@ -3,19 +3,11 @@ import random
 
 class LRU:
 
-    def __init__(self):
-        self.ListadeAccesos=[[1,1,500]
-        ,[1,2,1024]
-        ,[1,3,512]
-        ,[2,4,256]
-        ,[2,5,512]
-        ,[3,6,128]
-        ,[3,7,1024]
-        ,[3,8,512]
-        ,[3,9,512]
-        ,[4,10,256]
-        ]
+    def __init__(self, simulador):
+        self.simulador = simulador
+        self.paginasMarcadas=[]
 
+<<<<<<< Updated upstream
         self.listaAccesosBarajados=[[1,1,500]
         ,[3,6,128]
         ,[1,2,1024]
@@ -73,15 +65,18 @@ class LRU:
         for pagina in self.RAMLRU:
             self.FragmentacionInternaLRU= FragmentacionInternaLRU + ((4000-pagina[2])/1000)
         return self.FragmentacionInternaLRU
+=======
+    def calcularFragmentacionInternaOpt(self):
+        self.FragmentacionInternaOpt=0
+        for pagina in self.simulador.RAM:
+            self.simulador.stats.FragmentacionInterna = self.simulador.stats.FragmentacionInterna + ((4000-pagina[2])/1000)
+        return self.simulador.stats.FragmentacionInterna
+>>>>>>> Stashed changes
 
     def calcularRAMUtilizadaYVRAM(self):
-        self.RAMUtilizada=len(self.RAMLRU*4)
-        self.VRAMUtilizada = len(self.VRAMLRU*4)
+        self.simulador.stats.RAMUtilizada=len(self.simulador.RAM*4)
+        self.simulador.stats.VRAMUtilizada = len(self.simulador.VRAM*4)
         
-    def agregarContadoresLRU(self):
-        for x in self.listaAccesosBarajados:
-            x+=[0]
-
     def sacarMayorLRU(self):
         lista=[]
         for x in self.RAMLRU:
@@ -91,50 +86,50 @@ class LRU:
 
     def run(self):
         print("Corriendo el print")
-        while(len(self.listaAccesosBarajados)>18):
-            if len(self.RAMLRU)<self.RAMSize:
-                siguiente=self.listaAccesosBarajados.pop(0)#[1,1,500]
-                if self.RAMLRU.count(siguiente)==0:
-                    if self.VRAMLRU.count(siguiente)>0:
-                        self.VRAMLRU.remove(siguiente)
-                        self.RAMLRU.append(siguiente)
-                        self.TiempoSimLRU = self.TiempoSimLRU+5 # no me quedo claro que es
-                        self.TiempoTrashingLRU = self.TiempoTrashingLRU+5 # no me queda claro si en LRU hay Trashing
-                    if len(self.RAMLRU)!=0:
-                        for pagina in self.RAMLRU:
-                            pagina[3]+=1
-                    siguiente[3]+=1        
-                    self.RAMLRU.append(siguiente)
-                    self.TiempoSimLRU = self.TiempoSimLRU + 1
-                elif self.RAMLRU.count(siguiente)!=0:
-                    for pag in self.RAMLRU:
-                        print("------------")
-                        print(pag)
-                        print("------------")
-                        if pag==siguiente:
-                            pag[3]=0
-                    self.TiempoSimLRU = self.TiempoSimLRU + 1
+        while(len(self.simulador.varasBarajadas)>1):
+            if len(self.simulador.RAM) < self.RAMSize:
+                siguiente=self.simulador.varasBarajadas.pop(0)
+                if self.simulador.RAM.count(siguiente)==0:
+                    if self.simulador.VRAM.count(siguiente)>0:
+                        self.simulador.VRAM.remove(siguiente)
+                        self.simulador.RAM.append(siguiente)
+                        self.simulador.stats.TiempoSimulado = self.simulador.stats.TiempoSimulado+5
+                        self.simulador.stats.TiempoTrashing = self.simulador.stats.TiempoTrashing+5
+                    if len(self.RAM)!=0:
+                        for pagina in self.RAM:
+                            pagina.contador+=1
+                    siguiente.contador+=1        
+                    self.RAM.append(siguiente)
+                    self.simulador.stats.TiempoSimulado = self.simulador.stats.TiempoSimulado+1
+                elif self.RAM.count(siguiente)!=0:
+                    for pag in self.RAM:
+                        if pag.pid == siguiente.pid:
+                            pag.contador=0
+                    self.simulador.stats.TiempoSimulado = self.simulador.stats.TiempoSimulado+1
 
-            if len(self.RAMLRU) >= self.RAMSize:
-                siguiente= self.listaAccesosBarajados.pop(0)
+            if len(self.RAM) >= self.RAMSize:
+                siguiente=self.simulador.varasBarajadas.pop(0)
 
-                if self.RAMLRU.count(siguiente)==0: 
+                if self.RAM.count(siguiente)==0: 
                     mayor=self.sacarMayorLRU()
                     cont=0
-                    for pagina in self.RAMLRU:
-                        if pagina[3]==mayor:
-                            siguiente[3]+=1  
-                            self.RAMLRU[cont]=siguiente
-                            self.VRAMLRU.append(pagina)
+                    for pagina in self.RAM:
+                        if pagina.contador==mayor:
+                            siguiente.contador+=1  
+
+                            self.RAM[cont]=siguiente
+
+                            self.VRAM.append(pagina)
                         cont+=1
-                    if len(self.RAMLRU)!=0:
-                        for pagina in self.RAMLRU:
-                            pagina[3]+=1    
-                elif self.RAMLRU.count(siguiente)==0:
-                    for pag in self.RAMLRU:
-                        print("------------")
-                        print(pag)
-                        print("------------")
-                        if pag==siguiente:
-                            pag[3]=0
-                    self.TiempoSimLRU = self.TiempoSimLRU+1
+
+                    if len(self.RAM)!=0:
+                        for pagina in self.RAM:
+                            pagina.contador+=1 
+
+
+                elif self.RAM.count(siguiente)==0:
+                    for pag in self.RAM:
+                        if pag.PID==siguiente.PID:
+                            pag.contador=0
+                    self.simulador.stats.TiempoSimulado = self.simulador.stats.TiempoSimulado+1
+1
--- General/Pagina.py
+++ General/Pagina.py
@@ -3,3 +3,4 @@ class Pagina:
         self.PID = None
         self.Ptr = None
         self.Size = None
+        self.Contador=None

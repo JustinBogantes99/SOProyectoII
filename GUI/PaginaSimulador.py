35d0edd1eff27f1b06d8c4d7ee98b3dd6a63ea66
Justin Bogantes <justin.bogantes99@gmail.com>
2022-11-12 10:18:29 -0600

    Visualizar tabla con scroll bar

--- GUI/PaginaSimulador.py
+++ GUI/PaginaSimulador.py
@@ -17,6 +17,41 @@ class PaginaSimulador(tk.Frame):
         )
         txt_button.pack(side="bottom", fill=tk.X)
 
+        representarValores= [ " Page ID ","PID","LOADED","L-ADDR","M-ADDR","LOADED-T","Mark"]
+        frme_venta_optimo=tk.Frame(self, parent,width=400,height=300,bg="#FFE4B5")
+        frme_venta_optimo.place(x=20,y=100)
+
+        frme_venta_algoritmo=tk.Frame(self, parent,width=400,height=300,bg="blue")
+        frme_venta_algoritmo.place(x=750,y=100)
+        
+        labelsOpt=[]
+        labelsAlgo=[]
+
+        for i in range(len(representarValores)):
+            labelsOpt+=[tk.Label(frme_venta_optimo,text=representarValores[i],bd=30,bg="#FFE4B5")]
+        
+        fila=0
+        columna=0       
+        for x in labelsOpt:
+            x.grid(row=fila,column=columna) 
+            columna+=1
+
+        for j in range(len(representarValores)):
+            labelsAlgo+=[tk.Label(frme_venta_algoritmo,text=representarValores[j],bd=30,bg="#FFE4B5")]
+        
+        fila=0
+        columna=0       
+        for y in labelsAlgo:
+            y.grid(row=fila,column=columna) 
+            columna+=1    
+      
+
+        scrollbarOptimo = tk.Scrollbar(frme_venta_optimo, orient=tk.VERTICAL, command=labelsOpt)
+        scrollbarOptimo.place(relx=1, rely=0, relheight=1, anchor=tk.NE)
+        
+        scrollbarAlgoritmo = tk.Scrollbar(frme_venta_algoritmo, orient=tk.VERTICAL, command=labelsAlgo)
+        scrollbarAlgoritmo.place(relx=1, rely=0, relheight=1, anchor=tk.NE)
+        
         optimo_button = tk.Button(
             self,
             text="Correr optimo",
@@ -58,6 +93,9 @@ class PaginaSimulador(tk.Frame):
             command=lambda: controller.show_frame(PaginaDebugger),
         )
         switch_window_button.pack(side="bottom", fill=tk.X)
+
+    def acomodar(self,):
+        pass
 
     def test(self):
         print(self.controller.fileContent)

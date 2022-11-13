0a607c1f9bf1506b2b3200e19910a1bd61683e4d
Justin Bogantes <justin.bogantes99@gmail.com>
2022-11-12 21:30:55 -0600

    Random y Optimo "Funcionando"

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ def main():
     # gui = Windows()
     # gui.mainloop()
 
-    # simulador_optimo = Simulador("LRU", None)
-    # simulador_optimo.correr_algoritmo()
+    #simulador_optimo = Simulador("LRU", None)
+    #simulador_optimo.correr_algoritmo()
 
     # simulador_optimo = Simulador("SecondChance", None)
     # simulador_optimo.correr_algoritmo()
 
-    simulador_optimo = Simulador("Optimo", None)
+    #simulador_optimo = Simulador("Optimo", None)
+    #simulador_optimo.correr_algoritmo()
+
+    simulador_optimo = Simulador("Random", None)
     simulador_optimo.correr_algoritmo()
 
 
